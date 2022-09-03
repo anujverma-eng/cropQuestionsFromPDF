6562122095e0c85991229b204014a3f163148115
Anuj Verma <anujverma123ok@gmail.com>
2022-09-03 14:05:08 +0530

    cropQuestions

--- cropQuestions.py
+++ cropQuestions.py
@@ -4,7 +4,7 @@ import time
 import psutil
 
 
-def extractquestion(page_no,qn_no,fileName):
+def extractquestion(page_no,qn_no,fileName,ssName,showMe,zoom):
     doc = fitz.open(fileName)
     page = doc.load_page(page_no)
     blocks=page.get_text('dict',flags=0)['blocks']
@@ -28,32 +28,38 @@ def extractquestion(page_no,qn_no,fileName):
     point = [blocks[blk]['lines'][line]['spans'][span]['bbox']for blk,line,span in useful_block_line_span]
     fileSaveError="NO"
     try:
-        x0=min([i[0]for i in point])+10
+        x0=min([i[0]for i in point])+12
         y0=min([i[1]for i in point])
         x1=max([i[2]for i in point])+10
         y1=max([i[3]for i in point])
         # print(x1-x0,y1-y0)
     except:
+        fileSaveError=qn_no
         print("")
         print("************** Error ***************")
         print("")
         print(f"Error in Question ={qn_no}")
-        fileSaveError=qn_no
+        print("")
 
 
     try:
         if (fileSaveError=="NO" ):
-            mat = fitz.Matrix(2.0,2.0)
-            page.get_pixmap(clip=(x0,y0,x1,y1),matrix=mat).save(f'{fileName}_Q{qn_no}.png')
-            img = Image.open(f'{fileName}_Q{qn_no}.png')
-            img.show()
-            for proc in psutil.process_iter():
-                if proc.name() == "Microsoft.Photos.exe":
-                    # time.sleep(0.02)
-                    proc.kill()
+            mat = fitz.Matrix(zoom,zoom)
+            page.get_pixmap(clip=(x0,y0,x1,y1),matrix=mat).save(f'{ssName}{qn_no:03}.png')
+            if(showMe=="Y"):
+                try:
+                    img = Image.open(f'{ssName}{qn_no:03}.png')
+                    img.show()
+                    for proc in psutil.process_iter():
+                        if proc.name() == "Microsoft.Photos.exe":
+                            # time.sleep(0.02)
+                            proc.kill()
+                except:
+                    print("Unable to show Image!")
+                    
         else:
             mat = fitz.Matrix(2.0,2.0)
-            page.get_pixmap(clip=(x0,y0,x1,y1),matrix=mat).save(f'{fileName}_Q{qn_no}_ERROR.png')
+            page.get_pixmap(clip=(x0,y0,x1,y1),matrix=mat).save(f'{ssName}{qn_no:03}_ERROR.png')
     except:
         print("")
         print("*********** Warning !! ********************")
@@ -65,15 +71,20 @@ print("Happy cropping !!")
 print("")
 q_done=1
 fileName=input("Enter name of PDF file = ")
+ssName = input("Enter name of ScreenShots you want to save = ")
+print("")
+showMe = input("Do you want to see Images ? (Enter 'Y' for Yes & 'N' for No) = ")
+zoom = int(input("Enter the Zoom Factor = "))
+print("")
 pages = int(input("Enter total number of pages (Having Questions Only) = "))
 for page in range(pages):
     questions = int(input(f"Enter the Number of questions in Page - {page+1} = "))+1
 
     for q in range(q_done,questions):
-        extractquestion(page, q,fileName)
+        extractquestion(page, q,fileName,ssName,showMe,zoom)
         q_=q
     q_done=q_
 print("Thank you for Using !!")
 print("           -- Anuj Verma")
-time.sleep(8)
+time.sleep(15)
 print("Good Bye!!")
